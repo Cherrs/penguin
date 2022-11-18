1308dac6e0e9c145acbf05f242d26257024b3b57
Cherrs <lovef@outlook.com>
2022-11-19 03:12:49 +0800

    添加并使用Bytes crate

--- src/message_client.rs
+++ src/message_client.rs
@@ -1,6 +1,5 @@
-use std::str::Bytes;
-
 use async_trait::async_trait;
+use bytes::Bytes;
 
 #[async_trait]
 pub trait MessageClient {
@@ -8,4 +7,8 @@ pub trait MessageClient {
     async fn init() -> Self;
     /// 获取登录二维码
     async fn get_login_qrcode(&self) -> Bytes;
+
+    async fn get_qrcode_state(&self, sig: Bytes) -> QrcodeState;
 }
+
+pub enum QrcodeState {}
