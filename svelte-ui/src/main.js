b8c9792f3ef063c5924d9a65fc21b1398e489fdf
Cherrs <lovef@outlook.com>
2022-11-21 03:25:17 +0800

    不使用Top-level await，vite不支持

--- svelte-ui/src/main.js
+++ svelte-ui/src/main.js
@@ -4,10 +4,11 @@ import App from './App.svelte'
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 var fontFile = new FontFace('SourceHanSansCN', 'url(../public/SourceHanSansHWSC-VF.otf.woff2)');
+let app;
+fontFile.load().then(() => { 
+  app = new App({
+    target: document.getElementById('app')
+  })
 
-await fontFile.load();
-const app = new App({
-  target: document.getElementById('app')
-})
-
+});
 export default app
