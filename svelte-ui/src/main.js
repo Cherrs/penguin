5d35fbc0ec4252939e73093d408f3b34a8637098
Cherrs <lovef@outlook.com>
2022-11-27 05:27:00 +0800

    修复字体打包

--- svelte-ui/src/main.js
+++ svelte-ui/src/main.js
@@ -1,11 +1,12 @@
 import './app.css'
 import App from './App.svelte'
+import font from '../public/SourceHanSansHWSC-VF.otf.woff2'
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-var fontFile = new FontFace('SourceHanSansCN', 'url(../public/SourceHanSansHWSC-VF.otf.woff2)');
+var fontFile = new FontFace('SourceHanSansCN', `url(${font})`);
 let app;
-fontFile.load().then(() => { 
+fontFile.load().then(() => {
   app = new App({
     target: document.getElementById('app')
   })
